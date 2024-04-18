1f8dc0d7fd7496adfed0d50cd44997e6886ff2e6
DarXik <davca749@gmail.com>
2024-04-18 21:46:48 +0200

    google drive

--- front/src/routes/(site)/add-set/+page.server.ts
+++ front/src/routes/(site)/add-set/+page.server.ts
@@ -1,18 +1,54 @@
 import type { Actions } from './$types';
-import { v2 as cloudinary } from 'cloudinary';
-import {PUBLIC_VITE_CLOUDINARY_API_KEY, PUBLIC_VITE_CLOUDINARY_API_SECRET, PUBLIC_VITE_CLOUDINARY_CLOUD_NAME} from "$env/static/public";
+import { google } from "googleapis";
+import apiKeys from "./apiKeyGoogle.json"
+
+
+async function authorize() {
+
+    const SCOPE = ["https://www.googleapis.com/auth/drive"]
+    const auth = new google.auth.JWT(
+        apiKeys.client_email,
+        "" || undefined,
+        apiKeys.private_key,
+        SCOPE
+    )
+
+    await auth.authorize()
+
+    return auth;
+}
+
+async function uploadFile(auth: any, fileID: string, file: any) {
+    return new Promise((resolve, reject) => {
+        const drive = google.drive({ version: 'v3', auth });
+
+        var fileMetadata = {
+            name: fileID,
+            parents: ["1bBscV_4FIfGKcs7HvQ2-Fa87Wq0EEHox"]
+        }
+
+        drive.files.create({
+            resource: fileMetadata,
+            media: {
+                body: file,
+                mimeType: "image/png"
+            },
+            fields: "id"
+
+        }, (err: any, file: any) => {
+            if (err) {
+                console.error(err);
+                return reject(err);
+            } else {
+                resolve(file);
+            }
+        })
+    })
+}
 
 export const actions = {
     addSet: async ({ request, cookies, locals }) => {
-        // cloudinary.config({
-        //     cloud_name: PUBLIC_VITE_CLOUDINARY_CLOUD_NAME,
-        //     api_key: PUBLIC_VITE_CLOUDINARY_API_KEY,
-        //     api_secret: PUBLIC_VITE_CLOUDINARY_API_SECRET,
-        //     secure: true
-        // })
-
         const formData = await request.formData();
-        console.log(formData);
 
         const name = formData.get("name")?.toString();
         const description = formData.get("description")?.toString();
@@ -24,12 +60,17 @@ export const actions = {
         const yearBought = formData.get("yearBought")?.toString();
         const isBought = !!formData.get("isBought");
 
-        const imageThumbnail = formData.get("imageThumbnail") as File;
+        await authorize().then(auth => {
+            const legoSetName = (name || "lego-thumbnail") + new Date().getMilliseconds();
+            const file = formData.get("imageThumbnail")
 
-        // const image = await cloudinary.uploader.upload(imageThumbnail, {
-        //     upload_preset: "lego",
-        //     folder: "lego",
-        // })
+            uploadFile(auth, legoSetName, file)
+                .then(uploadResult => {
+                    console.log(uploadResult.data.id)
+                })
+                .catch(console.error)
+        })
+            .catch(console.error)
 
         let newSet = await fetch("http://localhost:3000/api/v1/sets/add", {
             method: "POST",
