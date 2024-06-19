b7fc9f7124899c34dddd3e8e44ba00a8b5e7cb16
DarXik <davca749@gmail.com>
2024-06-19 15:40:46 +0200

    oprava

--- back/controllers/User/DeleteController.ts
+++ back/controllers/User/DeleteController.ts
@@ -29,9 +29,9 @@ const deleteAccount = async (req: Request, res: Response) => {
             // odpojí a smaže instrukce, které měl pouze uživatel ve svých vlastních setech
             for (const set of sets) {
                 if (set.usedBy.length == 1) {
-                    const instruction = await prisma.instructions.findUnique({ where: { id: set.id } })
+                    let instruction = await prisma.instructions.findUnique({ where: { id: set.id })
 
-                    // smaže instukci, pokud existuje
+                    // smaže instukci, pokud existuje - jen tu soukromou
                     if (instruction) {
                         try {
                             await fs.unlink(path.join(__dirname, `../../../uploads/instructions/${instruction.instructions}`))
@@ -43,41 +43,42 @@ const deleteAccount = async (req: Request, res: Response) => {
                         }
                     }
                 }
+            }
 
-                for (const attachment of attachments) {
+            for (const attachment of attachments) {
 
-                    // smaže obrázek, pokud existuje
-                    if (attachment.image != null) {
-                        console.log(attachment.image)
-                        try {
-                            await fs.unlink(path.join(__dirname, `../../../uploads/images/${attachment.image}`))
-                        }
-                        catch (err) {
-                            console.log(err)
-                            return res.status(500).send({ message: "could not find image when deleting" });
-                        }
+                // smaže obrázek, pokud existuje
+                if (attachment.image != null) {
+                    console.log(attachment.image)
+                    try {
+                        await fs.unlink(path.join(__dirname, `../../../uploads/images/${attachment.image}`))
                     }
-
-                    // smaže vždy daný set attachment
-                    await prisma.setAttachment.delete({ where: { id: attachment.id } })
-                }
-
-                for (const set of sets) {
-                    if (set.usedBy.length == 1) {
-                        await prisma.sets.delete({ where: { id: set.id } })
+                    catch (err) {
+                        console.log(err)
+                        return res.status(500).send({ message: "could not find image when deleting" });
                     }
                 }
-                // smaže sessions uživatele
-                await prisma.sessions.deleteMany({ where: { userId: verifiedUser.user.id } })
-                await prisma.users.delete({ where: { id: verifiedUser.user.id } })
 
-                return res.status(200).send({ message: "account deleted" })
+                // smaže vždy daný set attachment
+                await prisma.setAttachment.delete({ where: { id: attachment.id } })
             }
-        catch (err) {
-                console.log(err)
-                return res.status(500).send({ message: "could not delete account" })
+
+            for (const set of sets) {
+                if (set.usedBy.length == 1) {
+                    await prisma.sets.delete({ where: { id: set.id } })
+                }
             }
+            // smaže sessions uživatele
+            await prisma.sessions.deleteMany({ where: { userId: verifiedUser.user.id } })
+            await prisma.users.delete({ where: { id: verifiedUser.user.id } })
+
+            return res.status(200).send({ message: "account deleted" })
         }
+        catch (err) {
+            console.log(err)
+            return res.status(500).send({ message: "could not delete account" })
+        }
+    }
     else {
         console.log("user not verified")
         return res.status(404)
