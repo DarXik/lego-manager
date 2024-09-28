62708eec5275cfff83d8456fa1c3fb4de440bac3
DarXik <73177923+DarXik@users.noreply.github.com>
2024-09-28 09:36:34 +0200

    util

--- front/.svelte-kit/ambient.d.ts
+++ front/.svelte-kit/ambient.d.ts
@@ -30,10 +30,7 @@ declare module '$env/static/private' {
 	export const SECRET_ORIGIN: string;
 	export const SECRET_MEDIA_ORIGIN: string;
 	export const DATABASE_URL: string;
-	export const ACSetupSvcPort: string;
-	export const ACSvcPort: string;
 	export const ALLUSERSPROFILE: string;
-	export const AMDRMPATH: string;
 	export const APPDATA: string;
 	export const CHROME_CRASHPAD_PIPE_NAME: string;
 	export const COLORTERM: string;
@@ -41,12 +38,8 @@ declare module '$env/static/private' {
 	export const CommonProgramW6432: string;
 	export const COMPUTERNAME: string;
 	export const ComSpec: string;
-	export const DokanLibrary1: string;
-	export const DokanLibrary1_LibraryPath_x64: string;
-	export const DokanLibrary1_LibraryPath_x86: string;
 	export const DriverData: string;
-	export const EFC_10640: string;
-	export const EnableLog: string;
+	export const EFC_3940: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -110,8 +103,6 @@ declare module '$env/static/private' {
 	export const npm_package_type: string;
 	export const npm_package_version: string;
 	export const NUMBER_OF_PROCESSORS: string;
-	export const NVM_HOME: string;
-	export const NVM_SYMLINK: string;
 	export const OneDrive: string;
 	export const OneDriveConsumer: string;
 	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
@@ -130,10 +121,7 @@ declare module '$env/static/private' {
 	export const PROMPT: string;
 	export const PSModulePath: string;
 	export const PUBLIC: string;
-	export const RlsSvcPort: string;
 	export const SESSIONNAME: string;
-	export const STARSHIP_SESSION_KEY: string;
-	export const STARSHIP_SHELL: string;
 	export const SystemDrive: string;
 	export const SystemRoot: string;
 	export const TEMP: string;
@@ -144,7 +132,6 @@ declare module '$env/static/private' {
 	export const USERDOMAIN_ROAMINGPROFILE: string;
 	export const USERNAME: string;
 	export const USERPROFILE: string;
-	export const VIRTUAL_ENV_DISABLE_PROMPT: string;
 	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
@@ -186,10 +173,7 @@ declare module '$env/dynamic/private' {
 		SECRET_ORIGIN: string;
 		SECRET_MEDIA_ORIGIN: string;
 		DATABASE_URL: string;
-		ACSetupSvcPort: string;
-		ACSvcPort: string;
 		ALLUSERSPROFILE: string;
-		AMDRMPATH: string;
 		APPDATA: string;
 		CHROME_CRASHPAD_PIPE_NAME: string;
 		COLORTERM: string;
@@ -197,12 +181,8 @@ declare module '$env/dynamic/private' {
 		CommonProgramW6432: string;
 		COMPUTERNAME: string;
 		ComSpec: string;
-		DokanLibrary1: string;
-		DokanLibrary1_LibraryPath_x64: string;
-		DokanLibrary1_LibraryPath_x86: string;
 		DriverData: string;
-		EFC_10640: string;
-		EnableLog: string;
+		EFC_3940: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
@@ -266,8 +246,6 @@ declare module '$env/dynamic/private' {
 		npm_package_type: string;
 		npm_package_version: string;
 		NUMBER_OF_PROCESSORS: string;
-		NVM_HOME: string;
-		NVM_SYMLINK: string;
 		OneDrive: string;
 		OneDriveConsumer: string;
 		ORIGINAL_XDG_CURRENT_DESKTOP: string;
@@ -286,10 +264,7 @@ declare module '$env/dynamic/private' {
 		PROMPT: string;
 		PSModulePath: string;
 		PUBLIC: string;
-		RlsSvcPort: string;
 		SESSIONNAME: string;
-		STARSHIP_SESSION_KEY: string;
-		STARSHIP_SHELL: string;
 		SystemDrive: string;
 		SystemRoot: string;
 		TEMP: string;
@@ -300,7 +275,6 @@ declare module '$env/dynamic/private' {
 		USERDOMAIN_ROAMINGPROFILE: string;
 		USERNAME: string;
 		USERPROFILE: string;
-		VIRTUAL_ENV_DISABLE_PROMPT: string;
 		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 		VSCODE_GIT_ASKPASS_MAIN: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
